efb2f0bf33871097c272764c61f3f5c1786cd9c6
SPS <tk34moodle@tk34.mskobr.ru>
2021-10-10 17:07:49 +0300

    Задание 1

--- Less041021.py
+++ Less041021.py
@@ -1,33 +1,4 @@
-test_num = 10
-test_str = 'Sergey'
-
-# user_number = int(input('Write num ='))
-#
-# test_str = 'Hello'
-# count =0
-# while count < user_number:
-#     print(test_str, count)
-#     count += 1
-# a = 5
-# b = 6
-# if a>b:
-#     print(a-b)
-#     print('выполнилось')
-# else:
-#     (a)
-#     print('else')
-#
-
-# test_str_1 = "Hello"
-# test_str_2 = 'Скажи "Вот такие дела"'
-# print(test_str_2+test_str_1)
-# print(test_str_2*2)
-# test_bool = False
-# test_list = [test_int, test_float, test_str_1, test_str_2, test_bool]
-# test_tuple = (test_int, test_float, test_str_1, test_str_2, test_bool)
-# test_dict = {"key_1": 10, "key_2": 11}
-# print(test_list)
-# print(test_tuple)
-# print(type(test_tuple))
-
-
+str1 = input('Введите ваше имя ')
+ye2 = int(input('Я учился в школе, лет '))
+ye3 = int(input('Еще в институте, лет '))
+print(f"Меня зовут {str1} я учился {ye2+ye3} лет ")
